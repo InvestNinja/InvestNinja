4ee20231c36e5abf6eb34acd4c24e76236f4db2e
Romulo <rcianci@msn.com>
2016-11-16 17:42:59 -0200

    Esboço para criação de Indice no front

---
+++ InvestNinja/InvestNinja.WebApp/src/indice.ts
@@ -0,0 +1,12 @@
+﻿export class Indice {
+    done = false;
+    descricao: string;
+    codigo: string;
+    valorCotaInicial: number;
+
+    constructor(codigo, descricao: string, valorCotaInicial: number) {
+        this.codigo = codigo;
+        this.descricao = descricao;
+        this.valorCotaInicial = valorCotaInicial;
+    }
+}
