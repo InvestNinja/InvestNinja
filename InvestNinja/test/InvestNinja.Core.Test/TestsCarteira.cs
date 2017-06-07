10259888a87de023f005d292a3e16b618b9cb77b
Romulo AC <rcianci@gmail.com>
2017-06-06 23:37:51 -0300

    Ajuste do teste unitário de indices e criação do teste de carteira

---
+++ InvestNinja/test/InvestNinja.Core.Test/TestsCarteira.cs
@@ -0,0 +1,126 @@
+﻿using InvestNinja.Core.Domain;
+using System;
+using System.Linq;
+using Xunit;
+
+namespace InvestNinja.Core.Test
+{
+    public class TestsCarteira
+    {
+        private Carteira CreateCarteira() => new Carteira(new CarteiraInitializer("ITest", "Teste", 100.0, DateTime.Now, 1000.0));
+
+        [Fact]
+        public void TestCriacaoDeCarteira() 
+        {
+            Carteira Carteira = CreateCarteira();
+            Assert.Equal(10.0, Carteira.QtdCotasAtual, 2);
+            Assert.Equal(100.0, Carteira.ValorCotaAtual, 2);
+            Assert.Equal(1000.0, Carteira.Saldo, 2);
+            Assert.Equal(1000.0, Carteira.TotalAplicado, 2);
+            Assert.Equal(0.0, Carteira.VariacaoFinanceira, 2);
+            Assert.Equal(0.0, Carteira.VariacaoCotaPercentual, 2);
+        }
+
+        [Fact]
+        public void TestCriacaoDePrimeiroItemCarteira()
+        {
+            Carteira Carteira = CreateCarteira();
+            ItemCarteira item = Carteira.Itens.LastOrDefault();
+            Assert.Equal(1000.0, item.ValorMovimentacao, 2);
+            Assert.Equal(1000.0, item.Saldo, 2);
+            Assert.Equal(0.0, item.QtdCotasAnterior, 2);
+            Assert.Equal(10.0, item.QtdCotasMovimentacao, 2);
+            Assert.Equal(10.0, item.QtdCotasAtual, 2);
+            Assert.Equal(0.0, item.VariacaoCotaPercentual, 2);
+            Assert.Equal(0.0, item.VariacaoFinanceira, 2);
+            Assert.Equal(100.0, item.ValorCota, 2);
+        }
+
+        [Fact]
+        public void TesteCarteiraComAdicaoDeSegundoItemCarteiraSemMovimentacao()
+        {
+            Carteira Carteira = CreateCarteira();
+            Carteira.AddItem(DateTime.Now.AddDays(1), 0.0, 1100.0);
+            Assert.Equal(10.0, Carteira.QtdCotasAtual, 2);
+            Assert.Equal(110.0, Carteira.ValorCotaAtual, 2);
+            Assert.Equal(1100.0, Carteira.Saldo, 2);
+            Assert.Equal(1000.0, Carteira.TotalAplicado, 2);
+            Assert.Equal(100.0, Carteira.VariacaoFinanceira, 2);
+            Assert.Equal(1.1, Carteira.VariacaoCotaPercentual, 2);
+        }
+
+        [Fact]
+        public void TesteCriacaoDeSegundoItemCarteiraSemMovimentacao()
+        {
+            Carteira Carteira = CreateCarteira();
+            Carteira.AddItem(DateTime.Now.AddDays(1), 0.0, 1100.0);
+            ItemCarteira item = Carteira.Itens.LastOrDefault();
+            Assert.Equal(0.0, item.ValorMovimentacao, 2);
+            Assert.Equal(1100.0, item.Saldo, 2);
+            Assert.Equal(10.0, item.QtdCotasAnterior, 2);
+            Assert.Equal(0.0, item.QtdCotasMovimentacao, 2);
+            Assert.Equal(10.0, item.QtdCotasAtual, 2);
+            Assert.Equal(1.1, item.VariacaoCotaPercentual, 2);
+            Assert.Equal(100.0, item.VariacaoFinanceira, 2);
+            Assert.Equal(110.0, item.ValorCota, 2);
+        }
+
+        [Fact]
+        public void TesteCarteiraComAdicaoDeSegundoItemCarteiraComAplicacao()
+        {
+            Carteira Carteira = CreateCarteira();
+            Carteira.AddItem(DateTime.Now.AddDays(1), 100.0, 1200.0);
+            Assert.Equal(10.91, Carteira.QtdCotasAtual, 2);
+            Assert.Equal(110.0, Carteira.ValorCotaAtual, 2);
+            Assert.Equal(1200.0, Carteira.Saldo, 2);
+            Assert.Equal(1100.0, Carteira.TotalAplicado, 2);
+            Assert.Equal(100.0, Carteira.VariacaoFinanceira, 2);
+            Assert.Equal(1.1, Carteira.VariacaoCotaPercentual, 2);
+        }
+
+        [Fact]
+        public void TesteCriacaoDeSegundoItemCarteiraComAplicacao()
+        {
+            Carteira Carteira = CreateCarteira();
+            Carteira.AddItem(DateTime.Now.AddDays(1), 100.0, 1200.0);
+            ItemCarteira item = Carteira.Itens.LastOrDefault();
+            Assert.Equal(100.0, item.ValorMovimentacao, 2);
+            Assert.Equal(1200.0, item.Saldo, 2);
+            Assert.Equal(10.0, item.QtdCotasAnterior, 2);
+            Assert.Equal(0.91, item.QtdCotasMovimentacao, 2);
+            Assert.Equal(10.91, item.QtdCotasAtual, 2);
+            Assert.Equal(1.1, item.VariacaoCotaPercentual, 2);
+            Assert.Equal(100.0, item.VariacaoFinanceira, 2);
+            Assert.Equal(110.0, item.ValorCota, 2);
+        }
+
+        [Fact]
+        public void TesteCarteiraComAdicaoDeSegundoItemCarteiraComResgate()
+        {
+            Carteira Carteira = CreateCarteira();
+            Carteira.AddItem(DateTime.Now.AddDays(1), -100.0, 1000.0);
+            Assert.Equal(9.09, Carteira.QtdCotasAtual, 2);
+            Assert.Equal(110.0, Carteira.ValorCotaAtual, 2);
+            Assert.Equal(1000.0, Carteira.Saldo, 2);
+            Assert.Equal(900.0, Carteira.TotalAplicado, 2);
+            Assert.Equal(100.0, Carteira.VariacaoFinanceira, 2);
+            Assert.Equal(1.1, Carteira.VariacaoCotaPercentual, 2);
+        }
+
+        [Fact]
+        public void TesteCriacaoDeSegundoItemCarteiraComResgate()
+        {
+            Carteira Carteira = CreateCarteira();
+            Carteira.AddItem(DateTime.Now.AddDays(1), -100.0, 1000.0);
+            ItemCarteira item = Carteira.Itens.LastOrDefault();
+            Assert.Equal(-100.0, item.ValorMovimentacao, 2);
+            Assert.Equal(1000.0, item.Saldo, 2);
+            Assert.Equal(10.0, item.QtdCotasAnterior, 2);
+            Assert.Equal(-0.91, item.QtdCotasMovimentacao, 2);
+            Assert.Equal(9.09, item.QtdCotasAtual, 2);
+            Assert.Equal(1.1, item.VariacaoCotaPercentual, 2);
+            Assert.Equal(100.0, item.VariacaoFinanceira, 2);
+            Assert.Equal(110.0, item.ValorCota, 2);
+        }
+    }
+}
--- InvestNinja/test/InvestNinja.Core.Test/TestsIndice.cs
+++ InvestNinja/test/InvestNinja.Core.Test/TestsIndice.cs
@@ -1,6 +1,4 @@
-﻿using InvestNinja.Core.Data;
-using InvestNinja.Core.Domain;
-using InvestNinja.Data;
+﻿using InvestNinja.Core.Domain;
 using System;
 using System.Linq;
 using Xunit;
@@ -9,16 +7,13 @@ namespace InvestNinja.Core.Test
 {
     public class TestsIndice
     {
-        private Indice CreateIndice() => new Indice(new IndiceInitializer("ITest", "Teste", 100.0, DateTime.Now, 1000.0));
+        private Indice CreateIndice() => new Indice(new IndiceInitializer("ITest", "Teste", 100.0, DateTime.Now));
 
         [Fact]
         public void TestCriacaoDeIndice() 
         {
             Indice indice = CreateIndice();
-            Assert.Equal(10.0, indice.QtdCotasAtual, 2);
             Assert.Equal(100.0, indice.ValorCotaAtual, 2);
-            Assert.Equal(1000.0, indice.Saldo, 2);
-            Assert.Equal(1000.0, indice.TotalAplicado, 2);
             Assert.Equal(0.0, indice.VariacaoFinanceira, 2);
             Assert.Equal(0.0, indice.VariacaoCotaPercentual, 2);
         }
@@ -28,100 +23,29 @@ namespace InvestNinja.Core.Test
         {
             Indice indice = CreateIndice();
             ItemIndice item = indice.Itens.LastOrDefault();
-            Assert.Equal(1000.0, item.ValorMovimentacao, 2);
-            Assert.Equal(1000.0, item.Saldo, 2);
-            Assert.Equal(0.0, item.QtdCotasAnterior, 2);
-            Assert.Equal(10.0, item.QtdCotasMovimentacao, 2);
-            Assert.Equal(10.0, item.QtdCotasAtual, 2);
             Assert.Equal(0.0, item.VariacaoCotaPercentual, 2);
             Assert.Equal(0.0, item.VariacaoFinanceira, 2);
             Assert.Equal(100.0, item.ValorCota, 2);
         }
 
         [Fact]
-        public void TesteIndiceComAdicaoDeSegundoItemIndiceSemMovimentacao()
+        public void TesteIndiceComAdicaoDeSegundoItemIndice()
         {
             Indice indice = CreateIndice();
-            indice.AddItem(DateTime.Now.AddDays(1), 0.0, 1100.0);
-            Assert.Equal(10.0, indice.QtdCotasAtual, 2);
+            indice.AddItem(DateTime.Now.AddDays(1), 110.0);
             Assert.Equal(110.0, indice.ValorCotaAtual, 2);
-            Assert.Equal(1100.0, indice.Saldo, 2);
-            Assert.Equal(1000.0, indice.TotalAplicado, 2);
-            Assert.Equal(100.0, indice.VariacaoFinanceira, 2);
+            Assert.Equal(10.0, indice.VariacaoFinanceira, 2);
             Assert.Equal(1.1, indice.VariacaoCotaPercentual, 2);
         }
 
         [Fact]
-        public void TesteCriacaoDeSegundoItemIndiceSemMovimentacao()
+        public void TesteCriacaoDeSegundoItemIndice()
         {
             Indice indice = CreateIndice();
-            indice.AddItem(DateTime.Now.AddDays(1), 0.0, 1100.0);
+            indice.AddItem(DateTime.Now.AddDays(1), 110.0);
             ItemIndice item = indice.Itens.LastOrDefault();
-            Assert.Equal(0.0, item.ValorMovimentacao, 2);
-            Assert.Equal(1100.0, item.Saldo, 2);
-            Assert.Equal(10.0, item.QtdCotasAnterior, 2);
-            Assert.Equal(0.0, item.QtdCotasMovimentacao, 2);
-            Assert.Equal(10.0, item.QtdCotasAtual, 2);
             Assert.Equal(1.1, item.VariacaoCotaPercentual, 2);
-            Assert.Equal(100.0, item.VariacaoFinanceira, 2);
-            Assert.Equal(110.0, item.ValorCota, 2);
-        }
-
-        [Fact]
-        public void TesteIndiceComAdicaoDeSegundoItemIndiceComAplicacao()
-        {
-            Indice indice = CreateIndice();
-            indice.AddItem(DateTime.Now.AddDays(1), 100.0, 1200.0);
-            Assert.Equal(10.91, indice.QtdCotasAtual, 2);
-            Assert.Equal(110.0, indice.ValorCotaAtual, 2);
-            Assert.Equal(1200.0, indice.Saldo, 2);
-            Assert.Equal(1100.0, indice.TotalAplicado, 2);
-            Assert.Equal(100.0, indice.VariacaoFinanceira, 2);
-            Assert.Equal(1.1, indice.VariacaoCotaPercentual, 2);
-        }
-
-        [Fact]
-        public void TesteCriacaoDeSegundoItemIndiceComAplicacao()
-        {
-            Indice indice = CreateIndice();
-            indice.AddItem(DateTime.Now.AddDays(1), 100.0, 1200.0);
-            ItemIndice item = indice.Itens.LastOrDefault();
-            Assert.Equal(100.0, item.ValorMovimentacao, 2);
-            Assert.Equal(1200.0, item.Saldo, 2);
-            Assert.Equal(10.0, item.QtdCotasAnterior, 2);
-            Assert.Equal(0.91, item.QtdCotasMovimentacao, 2);
-            Assert.Equal(10.91, item.QtdCotasAtual, 2);
-            Assert.Equal(1.1, item.VariacaoCotaPercentual, 2);
-            Assert.Equal(100.0, item.VariacaoFinanceira, 2);
-            Assert.Equal(110.0, item.ValorCota, 2);
-        }
-
-        [Fact]
-        public void TesteIndiceComAdicaoDeSegundoItemIndiceComResgate()
-        {
-            Indice indice = CreateIndice();
-            indice.AddItem(DateTime.Now.AddDays(1), -100.0, 1000.0);
-            Assert.Equal(9.09, indice.QtdCotasAtual, 2);
-            Assert.Equal(110.0, indice.ValorCotaAtual, 2);
-            Assert.Equal(1000.0, indice.Saldo, 2);
-            Assert.Equal(900.0, indice.TotalAplicado, 2);
-            Assert.Equal(100.0, indice.VariacaoFinanceira, 2);
-            Assert.Equal(1.1, indice.VariacaoCotaPercentual, 2);
-        }
-
-        [Fact]
-        public void TesteCriacaoDeSegundoItemIndiceComResgate()
-        {
-            Indice indice = CreateIndice();
-            indice.AddItem(DateTime.Now.AddDays(1), -100.0, 1000.0);
-            ItemIndice item = indice.Itens.LastOrDefault();
-            Assert.Equal(-100.0, item.ValorMovimentacao, 2);
-            Assert.Equal(1000.0, item.Saldo, 2);
-            Assert.Equal(10.0, item.QtdCotasAnterior, 2);
-            Assert.Equal(-0.91, item.QtdCotasMovimentacao, 2);
-            Assert.Equal(9.09, item.QtdCotasAtual, 2);
-            Assert.Equal(1.1, item.VariacaoCotaPercentual, 2);
-            Assert.Equal(100.0, item.VariacaoFinanceira, 2);
+            Assert.Equal(10.0, item.VariacaoFinanceira, 2);
             Assert.Equal(110.0, item.ValorCota, 2);
         }
     }
