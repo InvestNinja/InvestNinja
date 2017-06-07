807372d92c418e6ef4d68637807a2ac615979b8b
Alexandre Silva <alexandre.silva@netshoes.com>
2017-06-06 22:36:34 -0300

    Incluindo CORS na aplicação

--- InvestNinja/src/InvestNinja.Web/Startup.cs
+++ InvestNinja/src/InvestNinja.Web/Startup.cs
@@ -34,6 +34,16 @@ namespace InvestNinja.Web
         // This method gets called by the runtime. Use this method to add services to the container
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy",
+                    builder => builder.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader()
+                    .AllowCredentials() 
+                );
+            });
+
             // Add framework services.
             services.AddApplicationInsightsTelemetry(Configuration);
 
@@ -43,6 +53,8 @@ namespace InvestNinja.Web
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
+            app.UseCors("CorsPolicy");
+
             loggerFactory.AddConsole(Configuration.GetSection("Logging"));
             loggerFactory.AddDebug();
 
