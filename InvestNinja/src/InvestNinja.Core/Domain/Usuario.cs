1659b67eb791d2d095fe3d677b79625e762ca872
Romulo AC <rcianci@gmail.com>
2017-06-07 00:52:06 -0300

    Criação de classe de usuário

---
+++ InvestNinja/src/InvestNinja.Core/Domain/Usuario.cs
@@ -0,0 +1,15 @@
+﻿using InvestNinja.Core.Data;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace InvestNinja.Core.Domain
+{
+    public class Usuario : IEntity
+    {
+        [BsonId]
+        public string UserName { get; set; }
+
+        public string Email { get; set; }
+
+        public string Nome { get; set; }
+    }
+}
